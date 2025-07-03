dd31bfdd27cb6df75d0dbc5f9fca621a7135c404
mary ann <114210058+maryannintech@users.noreply.github.com>
2025-07-03 16:06:14 +0800

    refactor: update Navbar button styles for consistency and improve layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,16 +14,16 @@ export function Navbar() {
             alt="Light bulb logo"
             className="w-18 rotate-20 relative ml-3"
           ></img>
-        </div>
-        <div className="nav-bar-buttons flex justify-evenly items-center mt-9">
+        </div >
+        <div className="nav-bar-buttons flex justify-center gap-4 items-center mt-4 flex-wrap">
           <Link to={"/"}>
-            <button className="cursor-pointer">Home</button>
+            <button className="nav-bar-button">Home</button>
           </Link>
           <Link to={"/Category"}>
-            <button className="cursor-pointer">Play Quiz</button>
+            <button className="nav-bar-button">Play Quiz</button>
           </Link>
           <Link to={"/CreateQuiz"}>
-            <button className="cursor-pointer">Create Quiz</button>
+            <button className="nav-bar-button">Create Quiz</button>
           </Link>
         </div>
       </div>
