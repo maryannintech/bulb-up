4b952f20bfa863c14471b7d574cae75f8c402258
mary ann <114210058+maryannintech@users.noreply.github.com>
2025-07-03 15:49:25 +0800

    Add color utility classes for orange and yellow text in Navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,31 @@
 import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
 
 export function Navbar() {
   return (
     <>
-      <div className="name-logo text-center">
-        <p className="name-nav-bar 2xl">
-          Bulb <span>Up</span>
-        </p>
-        <img src=""></img>
-      </div>
-      <div className="nav-bar-buttons flex justify-evenly">
-        <Link to={"/"}>
-          <button className="cursor-pointer">Home</button>
-        </Link>
-        <Link to={"/Category"}>
-          <button className="cursor-pointer">Play Quiz</button>
-        </Link>
-        <Link to={"/CreateQuiz"}>
-          <button className="cursor-pointer">Create Quiz</button>
-        </Link>
+      <div className="mt-10 relative">
+        <div className="name-logo text-center flex justify-center items-center">
+          <p className="name-nav-bar 2xl text-right text-6xl text-[#D95724]">
+            Bulb <span className="block text-yellow">Up</span>
+          </p>
+          <img
+            src="/images/light-bulb.png"
+            alt="Light bulb logo"
+            className="w-18 rotate-20 relative ml-3"
+          ></img>
+        </div>
+        <div className="nav-bar-buttons flex justify-evenly items-center mt-9">
+          <Link to={"/"}>
+            <button className="cursor-pointer">Home</button>
+          </Link>
+          <Link to={"/Category"}>
+            <button className="cursor-pointer">Play Quiz</button>
+          </Link>
+          <Link to={"/CreateQuiz"}>
+            <button className="cursor-pointer">Create Quiz</button>
+          </Link>
+        </div>
       </div>
     </>
   );
