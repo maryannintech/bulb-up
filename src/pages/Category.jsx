0a95bc1cfb21b65630696bf2f9177b70170987a0
mary ann <114210058+maryannintech@users.noreply.github.com>
2025-07-04 20:14:06 +0800

    feat: update Category component to include difficulty selection options for user preferences

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,7 +1,24 @@
 export function Category() {
   return (
     <>
-      <h1>Categories</h1>
+      <div className="mt-5">
+        <p className="text-center text-orange italic sm:text-xl font-medium">
+          Set your preference
+        </p>
+        <div>
+          <label>Set difficulty</label>
+          <select>
+            <option value="any-difficulty">Any difficulty</option>
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+          </select>
+          <select>
+            <option value="multiple-choice">Multiple Choice</option>
+            <option value="multiple-choice">True or False</option>
+          </select>
+        </div>
+      </div>
     </>
   );
 }
