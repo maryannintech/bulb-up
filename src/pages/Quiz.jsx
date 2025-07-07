b1ec0d1e2d0c60d3cbad9385cf5f1203897c115a
mary ann <114210058+maryannintech@users.noreply.github.com>
2025-07-07 16:48:02 +0800

    feat: save best score when quiz is completed

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -40,6 +40,12 @@ export function Quiz() {
 
   const isQuizCompleted = currentQuestion >= quizData.length;
 
+  useEffect(() => {
+    if (isQuizCompleted && quizData.length > 0) {
+      saveBestScore(currentScore);
+    }
+  }, [isQuizCompleted, currentScore, quizData.length, selectedCategory]);
+
   let lastFetched = 0;
 
   useEffect(() => {
